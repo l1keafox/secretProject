e38a0c4c04c68cd4126dd9213c3df61167d8c16e
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-09-16 20:45:40 -0600

    removing old files

--- models/index.js
+++ models/index.js
@@ -1,10 +1,3 @@
 const User = require('./User');
-const Group = require('./Group');
 
-Group.hasMany(User, {
-    foreignKey: 'groupId',
-});
-
-User.belongsTo(Group,{
-    foreignKey: 'groupId'
-})
+module.exports = {User}
