8f659bbd73250d03ac39deefcab3399b696369d2
Dragon Lore <tate.j.foster@gmail.com>
2022-09-23 09:48:06 -0600

    added more regex for filtering chat

--- public/js/languageFilter.js
+++ public/js/languageFilter.js
@@ -1,8 +1,17 @@
-const swears = [/\b\w*(fuck)\w*\b/gi, /\b(shit)\b/gi, /\b(bitch)\b/gi];
+const swears = [
+	/\b\w*(fuck)\w*\b/gi,
+	/\b(shit)\b/gi,
+	/\b(bitch)\b/gi,
+	/\b(ass)\b/gi,
+	/\b(asshole)\b/gi,
+	/\b(asswipe)\b/gi,
+	/\b(dipshit)\b/gi,
+	/\b(asshat)\b/gi,
+];
 
 const cleanSentence = (sentence) => {
 	for (i = 0; i < swears.length; i++) {
-		const newSentence = sentence.replaceAll(swears[i], "fluffy");
+		const newSentence = sentence.replaceAll(swears[i], "!@#!");
 		sentence = newSentence;
 	}
 	return sentence;
@@ -13,5 +22,3 @@ console.log(
 		"testing a lot of fucking things this piece of shit makes me feel like a little bitch"
 	)
 );
-
-module.exports = cleanSentence;
